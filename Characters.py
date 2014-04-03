34b10d17dc7227d7feabf96823a5fbd36d8302e9
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-04-03 14:23:08 -0400

    Split up the main file into class files.  Multiple people can now work at once

---
+++ Characters.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  3 14:16:43 2014
+
+@author: jacob
+"""
+
+
+class Characters(object):
+    def __init__(self,name,level, HP,strength,defense,agility,intelligence,movement,weaponrange):
+        self.name=name
+        self.level=level
+        self.HP=HP
+        self.strength=strength
+        self.defense=defense
+        self.agility=agility
+        self.intelligence=intelligence
+        self.movement=movement
+        self.weaponrange=weaponrange
+
---
+++ World.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  3 14:17:48 2014
+
+@author: jacob
+"""
+
+class world(object):
+    """Encodes game state."""
+    def __init__(self):
+        self.world = {}
+        self.characters=pygame.sprite.Group()
+        
+        self.populateBlocks()
+        self.populateCharaceters()
+        for x in range(0,swidth,ref):
+            for y in range(0,sheight,ref):
+                node = Node(self,x,y)
+                self.world[(node.x,node.y)] = node
+                
+        for x in range(0,swidth,ref):
+            for y in range(0,sheight,ref):
+                if x not in range(3*ref,swidth-3*ref,ref) or y not in range(3*ref,sheight-3*ref,ref):
+                    boundary = Wall(self,x,y)
+                    boundary.color = black
+                    self.world[(boundary.x,boundary.y)] = boundary
+                    
