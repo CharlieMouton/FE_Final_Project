81c39480f93de19ee8125f4810312806979b49a0
Julian <julian@julian-Latitude-E6420.(none)>
2014-04-05 22:06:11 -0400

    dss

--- Blocks.py
+++ Blocks.py
@@ -57,6 +57,47 @@ class Forest(pygame.sprite.Sprite):
         self.rect.y = y
         
         
+class Fortress(pygame.sprite.Sprite):
+    """This class encodes the state of the outer edge of the screen. It is just a black outer edge."""
+    def __init__ (self,x,y):
+        #Call the parent class (Sprite) constructor     
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/Fortress.PNG')
+        self.image = pygame.transform.scale(self.image, (SQUARELENGTH, SQUARELENGTH))
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
