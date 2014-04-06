971de686117a11c0caaded0db2c1cf5f68d3e32e
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-04-05 22:05:21 -0400

    Updating the Main file

--- Blocks.py
+++ Blocks.py
@@ -55,28 +55,3 @@ class Forest(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
