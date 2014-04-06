52f27c0d4ee72af42266b6bf7a339bc06e87e371
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-04-05 21:46:38 -0400

    Updating the Main file

--- Node.py
+++ Node.py
@@ -10,5 +10,5 @@ import Blocks
 class Node(Blocks.Blocks):
     """A node is a floor block of our character."""
     def __init__(self, x, y):
-        Blocks.__init__(self)
+        Blocks.__init__(x,y)
         
