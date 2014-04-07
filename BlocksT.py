daa38db8d588573a2b43006c37c9c921e20b124f
CharlieMouton <Charlie@Moutons.org>
2014-04-07 17:15:49 -0400

    Added Bob

---
+++ BlocksT.py
@@ -0,0 +1,60 @@
+import pygame
+from Global_variables import *
+
+class Blocks():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        self.length=SQUARELENGTH        
+        
+class Grass(pygame.sprite.Sprite):
+    """This class encodes the state of a grass"""
+    def __init__ (self,x,y):
+        #Call the parent class (Sprite) constructor     
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/grass.png')
+        self.image = pygame.transform.scale(self.image, (SQUARELENGTH, SQUARELENGTH))
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.movementcost=1
+    
+class Outeredge(pygame.sprite.Sprite):
+    """This class encodes the state of the outer edge of the screen. It is just a black outer edge."""
+    def __init__ (self,x,y):
+        #Call the parent class (Sprite) constructor     
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/grass.png')
+        self.image = pygame.transform.scale(self.image, (SQUARELENGTH, SQUARELENGTH))
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.movementcost=0
+            
+class Forest(pygame.sprite.Sprite):
+    """This class encodes the forest block type."""
+    def __init__ (self,x,y):
+        #Call the parent class (Sprite) constructor     
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/grass.png')
+        self.image = pygame.transform.scale(self.image, (SQUARELENGTH, SQUARELENGTH))
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.movementcost=2
+        
+class Fortress(pygame.sprite.Sprite):
+    """This class encodes the fortress block type."""
+    def __init__ (self,x,y):
+        #Call the parent class (Sprite) constructor     
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/grass.png')
+        self.image = pygame.transform.scale(self.image, (SQUARELENGTH, SQUARELENGTH))
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.movementcost=0
