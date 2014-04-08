ce4732a79c5570612688e3ed2f09426f3363117d
hzhu <hzhu@hzhu.olin.edu>
2014-04-07 22:07:11 -0400

    Final fix.

--- Node.py
+++
@@ -1,7 +0,0 @@
-import Blocks
-
-class Node(Blocks.Blocks):
-    """A node is a floor block of our character."""
-    def __init__(self, x, y):
-        Blocks.__init__(x,y)
-        
--- Wall.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import Blocks
-
-class Wall(Blocks.Blocks):
-    """ A basic wall structure that stops the character."""
-    def __init__(self, model, x, y):
-        Blocks.__init__(self, wallcolor, x, y)
