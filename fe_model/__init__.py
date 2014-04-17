9ee3ce8eb51b126ff821f75326030c43c30967ba
Julian <julian@julian-Latitude-E6420.(none)>
2014-04-17 13:38:42 -0400

    ds

--- fe_model/__init__.py
+++ fe_model/__init__.py
@@ -9,6 +9,7 @@ from var_scripts import *
 from fe_view import *
 from images import *
 
+
 class Model:
     """Encodes game state."""
     def __init__(self):
@@ -34,11 +35,18 @@ class Model:
         self.character[(350,350)] = character.Archer(self,location=(350,350), name='Julian', movement=3)        
         self.character[(400,450)] = character.Archer(self,location=(400,450), name='David', movement=4)
         self.character[(550,550)] = character.Archer(self,location=(550,550), name='Charlie', movement=5)
+        # print test_model.grid
+        # print test_model.grid[(500,500)]
 
-        # self.character[(350,350)].available_locations()
-        # self.character[(400,450)].available_locations()
-        # self.character[(550,550)].available_locations()
-        
+        self.character[(350,350)].available_locations()
+        self.character[(400,450)].available_locations()
+        self.character[(550,550)].available_locations()
+        # print self.character[(350,350)].surroundings((0,0))
+
+        # print self.character[(350,350)].availabilities
+        # print len(self.character[(350,350)].availabilities)
+        # print self.character[(350,350)]
+
     def populatePlayers(self):
         pass
         # self.character[(,)]=character.Archer(self,location=(150,150), name='Julian', movement=4)
@@ -53,27 +61,24 @@ class Model:
         that the character is moving.  'x' and 'y' must be the same length."""
         for i in range(len(x)):
             if i>0:
+
                 if self.character[(x[i],y[i])] == None:
                     self.character[(x[i],y[i])]=self.character[(x[i-1], y[i-1])]
-                    self.character[(x[i],y[i])].location=(x[i],y[i])
-                    moved=int((abs(x[i]-x[i-1])+abs(y[i]-y[i-1]))/50)
-                    self.character[(x[i],y[i])].movementleft-=moved
                     self.character[(x[i-1],y[i-1])]=None
                 else:
                     pass
+
+                
     
-    def battleCall(self,player1, player2):
-        player1.battle(player2)
-        if player1.CurrentHP == 0:
-            self.character[player1.location]=None
-        if player2.CurrentHP == 0:
-            self.character[player2.location]=None
-    
+
+
+    def callBattle(self, x1, y1, x2, y2):
+        self.character[(x1,y1)].Battle(self.character[(x2,y2)])    
     """
     def setupChar(self,classtype,x,y):
         self.character+=[character.Archer.]
 
-    """
+    """    
 
     def update(self):
         for point in self.character:
@@ -97,3 +102,4 @@ if __name__ == "__main__":
         for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
+
